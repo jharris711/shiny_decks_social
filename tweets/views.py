f26cf77a9a4f042374af8345c2a456e234166ea9
Josh Harris <49429928+jharris711@users.noreply.github.com>
2020-05-02 15:44:33 -0400

    add create tweet form (unstyled)

--- tweets/views.py
+++ tweets/views.py
@@ -2,11 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse, Http404, JsonResponse
 # Create your views here.
 from random import randint
+from .forms import TweetForm
 from .models import Tweet
 
 
 def home_view(request, *args, **kwargs):
     return render(request, "pages/home.html", context={}, status=200)
+
+
+def tweet_create_view(request, *args, **kwargs):
+    form = TweetForm(request.POST or None)
+    if form.is_valid():
+        obj = form.save(commit=False)
+        obj.save()
+        form = TweetForm()
+    return render(request, 'components/form.html', context={"form":form})
 
 
 def tweet_list_view(request, *args, **kwargs):
