a8c1770cf79896d2f84de2b1b02811813f5b0df7
Josh Harris <49429928+jharris711@users.noreply.github.com>
2020-05-03 13:32:29 -0400

    vanilla js errors

--- tweets/models.py
+++ tweets/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from random import randint
 
 # Create your models here.
 
@@ -8,3 +8,13 @@ class Tweet(models.Model):
     #id = models.AutoField(primary_key=True)
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
+
+    class Meta:
+        ordering = ['-id']
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "content": self.content,
+            "likes": randint(1, 150)
+        }
--- tweets/views.py
+++ tweets/views.py
@@ -17,19 +17,28 @@ def home_view(request, *args, **kwargs):
 def tweet_create_view(request, *args, **kwargs):
     form = TweetForm(request.POST or None)
     next_url = request.POST.get("next") or None
-
     if form.is_valid():
         obj = form.save(commit=False)
+        # do other form related logic
         obj.save()
+        if request.is_ajax():
+            return JsonResponse(obj.serialize(), status=201) # 201 == created items
         if next_url != None and is_safe_url(next_url, ALLOWED_HOSTS):
             return redirect(next_url)
         form = TweetForm()
-    return render(request, 'components/form.html', context={"form":form})
+    if form.errors:
+        return JsonResponse(form.errors, status=400)
+    return render(request, 'components/form.html', context={"form": form})
 
 
 def tweet_list_view(request, *args, **kwargs):
+    """
+    REST API VIEW
+    Consume by JavaScript or Swift/Java/iOS/Andriod
+    return json data
+    """
     qs = Tweet.objects.all()
-    tweets_list = [{"id": x.id, "content": x.content, "likes": randint(1, 150)} for x in qs]
+    tweets_list = [x.serialize() for x in qs]
     data = {
         "isUser": False,
         "response": tweets_list
@@ -38,16 +47,19 @@ def tweet_list_view(request, *args, **kwargs):
 
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
-    #REST API VIEW
+    """
+    REST API VIEW
+    Consume by JavaScript or Swift/Java/iOS/Andriod
+    return json data
+    """
     data = {
         "id": tweet_id,
     }
     status = 200
     try:
-        tweet = Tweet.objects.get(id=tweet_id)
-        data['content'] = tweet.content
+        obj = Tweet.objects.get(id=tweet_id)
+        data['content'] = obj.content
     except:
-        data['message'] = "Not Found"
+        data['message'] = "Not found"
         status = 404
-
-    return JsonResponse(data, status=status)
+    return JsonResponse(data, status=status) # json.dumps content_type='application/json'
