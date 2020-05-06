fd11acd7ac5de8f1d91b2657f94d03a9283ca62a
Josh Harris <49429928+jharris711@users.noreply.github.com>
2020-05-06 13:53:43 -0400

    create basic tweet component

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -19,6 +19,14 @@ function loadTweets(callback) {
   xhr.send()
 }
 
+function Tweet(props) {
+  const { tweet } = props;
+  const className = props.className ? props.className : 'col-10 mx-auto col-md-6 shadow-sm';
+  return <div className={className}>
+    <p>{ tweet.id } - { tweet.content }</p>
+  </div>
+}
+
 
 function App() {
   const [tweets, setTweets] = useState([])
@@ -42,11 +50,11 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>
-          {tweets.map((tweet, index)=>{
-            return <li>{tweet.content}</li>
+        <div>
+          {tweets.map((item, index)=>{
+            return <Tweet tweet={item} key={index} className='my-5 py-5 border bg-white text-dark'/>
           })}
-        </p>
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
