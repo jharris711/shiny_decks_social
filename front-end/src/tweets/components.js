1ebfa869a85e1f8b43a1f5fe916f43d327b5ba95
Josh Harris <49429928+jharris711@users.noreply.github.com>
2020-05-06 18:12:18 -0400

    improve action button

--- front-end/src/tweets/components.js
+++ front-end/src/tweets/components.js
@@ -27,21 +27,37 @@ export function TweetsList(props) {
 
 export function ActionBtn(props) {
     const {tweet, action} = props;
+    const [likes, setLikes] = useState(tweet.likes ? tweet.likes : 0);
+    const [userLike, setUserLike] = useState(tweet.userLike === true ? true : false);
     const className = props.className ? props.className : 'btn btn-primary btn-sm'
-    return action.type === 'like' ? <button className={className}>{tweet.likes} Likes</button> : null
+    const actionDisplay = action.display ? action.display : 'Action'
+    
+    const handleClick = (event) => {
+      event.preventDefault();
+      if (action.type === 'like') {
+        if (userLike === true) {
+            setLikes(likes - 1);
+            setUserLike(false);
+        } else {
+            setLikes(likes + 1);
+            setUserLike(true);
+        }
+      }
+    };
+    const display = action.type === 'like' ? `${likes} ${actionDisplay}` : actionDisplay
+    return <button className={className} onClick={handleClick}>{display}</button>
 };
   
   
 export function Tweet(props) {
     const { tweet } = props;
     const className = props.className ? props.className : 'col-10 mx-auto col-md-6 shadow-sm';
-    
     return <div className={className}>
       <p>{ tweet.id } - { tweet.content }</p>
       <div className='btn btn-group'>
-        <ActionBtn tweet={tweet} action={ {type: "like"} } />
-        <ActionBtn tweet={tweet} action={ {type: "unlike"} } />
-        <ActionBtn tweet={tweet} action={ {type: "like"} } />
+        <ActionBtn tweet={tweet} action={ {type: "like", display:"Likes"} } />
+        <ActionBtn tweet={tweet} action={ {type: "unlike", display: "Unlike"} } />
+        <ActionBtn tweet={tweet} action={ {type: "retweet", display:"Retweet"} } />
       </div>
     </div>
 };
