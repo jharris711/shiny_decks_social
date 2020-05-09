546e3df898e3c380409d99b017c0bcb0fc7551c1
Josh Harris <49429928+jharris711@users.noreply.github.com>
2020-05-09 15:17:58 -0400

    update action btn

--- front-end/src/tweets/lookup.js
+++ front-end/src/tweets/lookup.js
@@ -5,6 +5,14 @@ export function apiTweetCreate(newTweet, callback) {
     backendLookup('POST', '/tweets/create/', callback, {content: newTweet});
 };
 
+export function apiTweetAction(tweet_id, action, callback) {
+    const data = {
+        id: tweet_id,
+        action: action
+    }
+    backendLookup('POST', '/tweets/action/', callback, data);
+};
+
 //Tweet Lookup function
 export function apiTweetList(callback) {
     backendLookup('GET', '/tweets/', callback);
