989d329b57eed889559bc62a05779b32ffae2ef2
Josh Harris <49429928+jharris711@users.noreply.github.com>
2020-05-09 14:27:36 -0400

    handle new tweet react/django

--- front-end/src/tweets/components.js
+++ front-end/src/tweets/components.js
@@ -3,39 +3,40 @@ import React, {useEffect, useState}  from 'react'
 import {createTweet, loadTweets} from '../lookup'
 
 export function TweetsComponent(props) {
-    const textAreaRef = React.createRef();
-    const [newTweets, setNewTweets] = useState([]);
-    const handleSubmit = (event) => {
-      event.preventDefault();
-      const newVal = textAreaRef.current.value;
-      let tempNewTweets = [...newTweets];
-      // change this to a server side call
-      createTweet(newVal, (response, status) => {
-        if (status === 201) {
-          tempNewTweets.unshift({
-            content: newVal,
-            likes: 0,
-            id: 12313
-          });
-        } else {
-          console.log(response)
-          alert("An error occurred. Please try again.")
-        }
-      });
-      setNewTweets(tempNewTweets)
-      textAreaRef.current.value = ''
-    };
-    return <div className={props.className}>
-            <div className='col-12 mb-3'>
-              <form onSubmit={handleSubmit}>
-                <textarea ref={textAreaRef} required={true} className='form-control' name='tweet'>
+  const textAreaRef = React.createRef();
+  const [newTweets, setNewTweets] = useState([]);
 
-                </textarea>
-                <button type='submit' className='btn btn-primary my-3'>Tweet</button>
-            </form>
-            </div>
-        <TweetsList newTweets={newTweets} />
+  const handleBackendUpdate = (response, status) => {
+    //Backend API Response handler
+    let tempNewTweets = [...newTweets];
+    if (status === 201){
+      tempNewTweets.unshift(response);
+      setNewTweets(tempNewTweets);
+    } else {
+      console.log(response);
+      alert("An error occured please try again");
+    }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newVal = textAreaRef.current.value;
+    //Backend API Request handler
+    createTweet(newVal, handleBackendUpdate);
+    textAreaRef.current.value = '';
+  };
+
+  return <div className={props.className}>
+    <div className='col-12 mb-3'>
+      <form onSubmit={handleSubmit}>
+        <textarea ref={textAreaRef} required={true} className='form-control' name='tweet'>
+
+        </textarea>
+        <button type='submit' className='btn btn-primary my-3'>Tweet</button>
+      </form>
     </div>
+    <TweetsList newTweets={newTweets} />
+  </div>
 };
 
 export function TweetsList(props) {
@@ -56,7 +57,7 @@ export function TweetsList(props) {
           setTweetsInit(response);
           setTweetsDidSet(true);
         } else {
-          alert("There was an error")
+          alert("There was an error");
         }
       };
       loadTweets(myCallback);
@@ -69,39 +70,40 @@ export function TweetsList(props) {
 
 
 export function ActionBtn(props) {
-    const {tweet, action} = props
-    const [likes, setLikes] = useState(tweet.likes ? tweet.likes : 0)
-    const [userLike, setUserLike] = useState(tweet.userLike === true ? true : false)
-    const className = props.className ? props.className : 'btn btn-primary btn-sm'
-    const actionDisplay = action.display ? action.display : 'Action'
+  const {tweet, action} = props;
+  const [likes, setLikes] = useState(tweet.likes ? tweet.likes : 0);
+  const [userLike, setUserLike] = useState(tweet.userLike === true ? true : false);
+  const className = props.className ? props.className : 'btn btn-primary btn-sm';
+  const actionDisplay = action.display ? action.display : 'Action';
     
-    const handleClick = (event) => {
-      event.preventDefault()
-      if (action.type === 'like') {
-        if (userLike === true) {
-          // perhaps i Unlike it?
-          setLikes(likes - 1)
-          setUserLike(false)
-        } else {
-          setLikes(likes + 1)
-          setUserLike(true)
-        }
-        
+  const handleClick = (event) => {
+    event.preventDefault();
+    if (action.type === 'like') {
+      if (userLike === true) {
+        // perhaps i Unlike it?
+        setLikes(likes - 1);
+        setUserLike(false);
+      } else {
+        setLikes(likes + 1);
+        setUserLike(true);
       }
     }
-    const display = action.type === 'like' ? `${likes} ${actionDisplay}` : actionDisplay
-    return <button className={className} onClick={handleClick}>{display}</button>
+  };
+  const display = action.type === 'like' ? `${likes} ${actionDisplay}` : actionDisplay;
+
+  return <button className={className} onClick={handleClick}>{display}</button>
 };
   
 export function Tweet(props) {
-    const {tweet} = props
-    const className = props.className ? props.className : 'col-10 mx-auto col-md-6'
-    return <div className={className}>
-        <p>{tweet.id} - {tweet.content}</p>
-        <div className='btn btn-group'>
-          <ActionBtn tweet={tweet} action={{type: "like", display:"Likes"}}/>
-          <ActionBtn tweet={tweet} action={{type: "unlike", display:"Unlike"}}/>
-          <ActionBtn tweet={tweet} action={{type: "retweet", display:""}}/>
-        </div>
+  const {tweet} = props;
+  const className = props.className ? props.className : 'col-10 mx-auto col-md-6';
+
+  return <div className={className}>
+    <p>{tweet.id} - {tweet.content}</p>
+    <div className='btn btn-group'>
+      <ActionBtn tweet={tweet} action={{type: "like", display:"Likes"}}/>
+      <ActionBtn tweet={tweet} action={{type: "unlike", display:"Unlike"}}/>
+      <ActionBtn tweet={tweet} action={{type: "retweet", display:""}}/>
     </div>
+  </div>
 };
